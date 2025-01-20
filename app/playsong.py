c38b623c63a6bbdb21c0bd34468cce8cb575a8fc
ultraorth <121553971+ultraorth@users.noreply.github.com>
2025-01-20 12:20:43 +0200

    Initial commit

---
+++ app/playsong.py
@@ -0,0 +1,48 @@
+VERSION = "1.0.0"
+import webbrowser
+import argparse
+from googleapiclient.discovery import build
+
+API_KEY = "AIzaSyBUX24jWciPri05mO_i7DlvjA5BlfV7SNo"
+
+def get_youtube_video_url(song_name):
+    #API client
+    youtube = build("youtube", "v3", developerKey=API_KEY)
+
+    #Search for song
+    request = youtube.search().list(
+            q=song_name,
+            part="snippet",
+            type="video",
+            maxResults=1
+        )
+
+    response = request.execute()
+
+    if "items" in response and len(response["items"]) > 0:
+        video_id = response["items"][0]["id"]["videoId"]
+        return f"https://www.youtube.com/watch?v={video_id}"
+    else:
+        raise Exception("No results found for the song.")
+
+def main():
+    #parser
+    parser = argparse.ArgumentParser(description="Play song on Youtube.")
+    parser.add_argument("command", choices=["playsong", "play"], help="Command to execute")
+    parser.add_argument("song", nargs="+", help="Name of the song to play")
+    parser.add_argument("--version", action="version", version=f"playsong v{VERSION}")
+    args = parser.parse_args()
+
+    song_name = " ".join(args.song)
+
+    try:
+        video_url = get_youtube_video_url(song_name)
+
+        print(f"Now playing: {video_url}")
+        webbrowser.open(video_url)
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
+
